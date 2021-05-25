791f15fcdbe6b36a876a458a2c6f1b21dfbbb399
Anthony Sim <anthonyysim@gmail.com>
2021-05-25 12:19:28 -0700

    set up express server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
 
+// CORS
+app.use(cors());
 
+// shows static files react index.html
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT} 🐵!`)
+})
