4fce29f72ddfca7f971bc3f1d6947b7b98e272e8
Anthony Sim <anthonyysim@gmail.com>
2021-05-25 20:54:28 -0700

    react router

--- challenge_2/client/src/index.jsx
+++ challenge_2/client/src/index.jsx
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'));
