590dcf796f236c3671a0728467c9d300c0f2e107
Anthony Sim <anthonyysim@gmail.com>
2021-05-25 21:10:28 -0700

    react router link switch and route

--- challenge_2/client/src/components/App.jsx
+++ challenge_2/client/src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import CurrentPrice from './CurrentPrice.jsx';
 import HistoricalPrice from './HistoricalPrice.jsx';
 import { Container } from 'react-bootstrap';
-
+import { Link, Switch, Route } from 'react-router-dom';
 
 import LineChart from './LineChart.jsx';
 
@@ -11,12 +11,18 @@ const App = () => {
   return (
     <div>
       <Container>
+        <Link to="/">Home</Link>
+        <Link to="/today">Current Price</Link>
+        <Link to="/one-month">1 Month Price</Link>
+        <Link to="/three-months">3 Month Price</Link>
 
+        <Switch>
+          <Route exact path="/" component={CurrentPrice} />
+          <Route exact path="/one-month" component={HistoricalPrice} />
+          <Route exact path="/three-months" component={LineChart} />
+        </Switch>
         <br />
         <br />
-        <CurrentPrice />
-        <LineChart />
-        {/* <HistoricalPrice /> */}
       </Container>
     </div>
   );
